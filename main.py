64352944f625c5f65c378d3c8ecef2288f4aa61a
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2023-11-13 22:01:19 +0000

    Create browser class and init method

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import re
+from typing import Final
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 
 # Regex used for finding e-mails in text
@@ -7,3 +12,17 @@ EMAIL_REGEX: Final[str] = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*
 a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[
 0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[
 \x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
+
+class Browser:
+    """Create a browser that scrapes any url for e-mail addresses"""
+
+    # Initialize the webdriver with the path to chromedriver.exe
+    def __init__(self, driver: str):
+        print('Starting up browser...')
+        self.chrome_options = Options()
+        self.chrome_options.add_argument("--headless")  # Prevents the browser from showing
+        self.chrome_options.add_argument("--disable-extensions")
+        self.chrome_options.add_argument("--disable-gpu")
+
+        self.service = Service(driver)
+        self.browser = webdriver.Chrome(service=self.service, options=self.chrome_options)
